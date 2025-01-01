24b2ed5de25b22f5cfe714626daad2c8f6f53ae7
Radhi <Radhi@192.168.1.6>
2025-01-01 19:17:52 +0530

    javasaved

--- Maven/src/main/java/automatiom/Sample.java
+++ Maven/src/main/java/automatiom/Sample.java
@@ -5,6 +5,25 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Sample a=new Sample();
+		Sample b=new Sample();		
+		a.add(3.5f,6.7f);
+		b.add(6.9f, 4.7f);
+		
+	}
+	public float add(float a,float b)
+	{
+	float c=a+b;
+	System.out.println(c);
+	return c;
+	}
+	public float sub(float a,float b)
+	{
+	float c=a-b;
+	System.out.println(c);
+	return c;
 	}
 
-}
+	}
+
+
